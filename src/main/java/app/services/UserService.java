75c8eb2a15d829fb291a44c06fbc76a9b6644e70
RikkeMariussen <cph-rm225@cphbusiness.dk>
2025-02-21 15:40:20 +0100

    Oprettet UserService som indeholder persistUser, som laver og gemmer en user i databasen ved at kalde på userDAO.create

---
+++ src/main/java/app/services/UserService.java
@@ -0,0 +1,30 @@
+package app.services;
+
+import app.daos.UserDAO;
+import app.dtos.UserDTO;
+import app.entities.Users;
+import jakarta.persistence.EntityManagerFactory;
+
+
+public class UserService
+{
+    private static EntityManagerFactory emf;
+    private final UserDAO userDAO;
+
+    public UserService(UserDAO userDAO)
+    {
+        this.userDAO = userDAO;
+    }
+
+    //Could this be void?
+    public Users persistUser(UserDTO userDTO)
+    {
+        Users user = Users.builder()
+                .name(userDTO.getName())
+                .age(userDTO.getAge())
+                .weight(userDTO.getWeight())
+                .build();
+        user = userDAO.create(user);
+        return user;
+    }
+}
