97558d018becef8ff328c40348d1709dea952e41
RikkeMariussen <cph-rm225@cphbusiness.dk>
2025-02-21 12:32:02 +0100

    CityInfo CRUD+getInstance

---
+++ src/main/java/app/daos/CityInfoDAO.java
@@ -0,0 +1,109 @@
+package app.daos;
+
+import app.entities.CityInfo;
+import app.entities.WeatherInfo;
+import app.exceptions.ApiException;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+
+import java.util.List;
+
+public class CityInfoDAO implements IDAO<CityInfo, Long>
+{
+
+    private static EntityManagerFactory emf;
+    private static CityInfoDAO INSTANCE;
+
+    //Constructoren - fordi det er singleton pattern, laver man en privat constructor så den er cuttet af og andre ikke kan bruge den
+    private CityInfoDAO(EntityManagerFactory emf)
+    {
+        this.emf = emf;
+    }
+
+    //Singleton pattern
+    public static CityInfoDAO getInstance(EntityManagerFactory emf)
+    {
+        if (INSTANCE == null)
+        {
+            INSTANCE = new CityInfoDAO(emf);
+        }
+        return INSTANCE;
+    }
+
+    @Override
+    public CityInfo create(CityInfo type)
+    {
+        try (EntityManager em = emf.createEntityManager())
+        {
+            em.getTransaction().begin();
+            em.persist(type);
+            em.getTransaction().commit();
+            return type;
+        } catch (Exception e)
+        {
+            throw new ApiException(401, "Error creating cityInfo", e);
+            //Eventuelt kunne man have en rollback
+        }
+    }
+
+    @Override
+    public CityInfo read(Long id)
+    {
+        try (EntityManager em = emf.createEntityManager())
+        {
+            return em.find(CityInfo.class, id);
+        }
+    }
+
+    @Override
+    public List<CityInfo> readAll()
+    {
+        try (EntityManager em = emf.createEntityManager())
+        {
+            return em.createQuery("SELECT c FROM CityInfo c", CityInfo.class).getResultList();
+        } catch (Exception e)
+        {
+            throw new ApiException(401, "Error finding list of city info", e);
+        }
+    }
+
+    @Override
+    public CityInfo update(CityInfo type)
+    {
+        try (EntityManager em = emf.createEntityManager())
+        {
+            em.getTransaction().begin();
+            //Merge har en returtype, hvorfor man skal koble det på et updated objekt
+            CityInfo updatedCityInfo = em.merge(type);
+            //Ryd cache
+            em.flush();
+            //Vi vil gerne sikre os det KUN er det ene objekt der er opdateret der kommer med
+            em.refresh(updatedCityInfo);
+            em.getTransaction().commit();
+            return updatedCityInfo;
+        } catch (Exception e)
+        {
+            throw new ApiException(401, "Error updating city information", e);
+        }
+    }
+
+    @Override
+    public void delete(Long id)
+    {
+        try (EntityManager em = emf.createEntityManager())
+        {
+            em.getTransaction().begin();
+            CityInfo cityInfo = em.find(CityInfo.class, id);
+            if (cityInfo == null)
+            {
+                em.getTransaction().rollback();
+                throw new ApiException(401, "Error deleting cityInfo, cityInfo was not found");
+            }
+            em.remove(cityInfo);
+            em.getTransaction().commit();
+        } catch (Exception e)
+        {
+            throw new ApiException(401, "Error removing cityInfo", e);
+        }
+    }
+}
