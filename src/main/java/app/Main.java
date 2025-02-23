4280d760c9a671df9611955debcd7b8328f3fc34
Gervig <gervig91@gmail.com>
2025-02-23 20:12:28 +0100

    testing in main

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,10 +1,18 @@
 package app;
 
+import app.callables.CityServiceCallable;
+import app.callables.WeatherServiceCallable;
 import app.config.HibernateConfig;
+import app.dtos.ActivityDTO;
+import app.dtos.CityInfoDTO;
 import app.dtos.UserDTO;
+import app.dtos.WeatherInfoDTO;
+import app.populators.ActivityDTOPopulator;
 import app.populators.UserDTOPopulator;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+
+import java.util.List;
 
 public class Main
 {
@@ -14,6 +22,16 @@ public class Main
         EntityManager em = emf.createEntityManager();
 
         UserDTO[] userDTOS = UserDTOPopulator.populate();
+
+        ActivityDTO[] activityDTOS = ActivityDTOPopulator.populate();
+
+        // Scalable can call multiple cities
+        String[] cityNames = new String[]{
+                "Roskilde"
+        };
+
+        List<CityInfoDTO> cityInfoDTOS = CityServiceCallable.getCityInfoDTOs(cityNames);
+        List<WeatherInfoDTO> weatherInfoDTOS = WeatherServiceCallable.getWeatherInfoDTOs(cityNames);
 
         // Close the database connection:
         em.close();
